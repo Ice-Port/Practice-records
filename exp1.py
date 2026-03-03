ab9dd4a712069ca0dbe9ed21543258c533e3e4a2
Ice-Port <2978605196@qq.com>
2026-03-03 16:36:16 +0800

    bugfix

--- exp1.py
+++ exp1.py
@@ -1,6 +1,7 @@
-for i in range(0,24,2):
-    if i>=0 & i<13:
+i=0
+if i>=0 & i<13:
+    for i in range(0,24,2):
         print("hello world"+str(i))
-    else:
-        print("This is my experiment code"+str(i))
+else:
+    print("This is my experiment code")
         
