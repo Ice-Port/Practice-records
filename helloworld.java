251133eacc3f4cf2fc805ac07c84e9ffc3a09f5e
Ice-Port <bundleIKD2570@outlook.com>
2026-04-17 17:48:47 +0800

    version 2

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,19 @@
-public class helloworld{
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class helloworld implements Hello{
+    @Override
+    public void Hello(){
+        System.out.println("Hello");
     }
+    @Override
+    public void World(){
+        System.out.println("world!");
+    }
+    public static void main(String[] args) {
+        helloworld Helloworld = new helloworld();
+        Helloworld.Hello();
+        Helloworld.World();
+    }
+}
+interface Hello{
+    void Hello();
+    void World();
 }
